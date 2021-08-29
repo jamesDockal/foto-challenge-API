cc0497585cd104707b0fb436a295f0f934907992
james dockal <jamesistranho@gmail.com>
2021-08-29 05:21:16 -0300

    feat: create sqlite images table with migration

---
+++ src/database/index.ts
@@ -0,0 +1,6 @@
+import { createConnection } from "typeorm";
+
+// make the connection with the db
+// with the config of ormconfig.json
+
+createConnection();
---
+++ src/database/migrations/1630199183590-Images.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Images1630199183590 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: "images",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            isPrimary: true,
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: "image",
+            type: "mediumblob",
+            isNullable: false,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable("images");
+  }
+}
